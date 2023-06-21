04639bdf222cf02ce8c1ab70cc9a4f40c6bf1591
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-06-21 14:53:23 +0700

    commit while

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -19,7 +19,7 @@ int main() {
 		//mendapatkan setiap karakter dalam satu baris
 		getline(cin, baris);
 		//Loop akan berhenti jika anda memasukkan karakter q
-		if (baris == "q") break;
+		if (baris == "q") break; 
 		//menulis dan memasukan nilai dari 'baris' ke dalam file
 		outfile << baris << endl;
 	}
